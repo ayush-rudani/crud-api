6d6242f9a35d17d6b05d078fe34b4cfe1be88114
Ayush Rudani <ayushcrudani@gmail.com>
2023-05-17 00:17:19 +0530

    M order model

---
+++ models/Orders.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const orderSchema = new mongoose.Schema({
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    products: [{ type: Schema.Types.ObjectId, ref: "Product" }],
+    totalCost: { type: Number, required: true },
+    createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Order", productSchema);
